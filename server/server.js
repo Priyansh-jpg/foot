04508fd0ba554a68d41fd2c0048dbbce419c1392
Priyansh-jpg <priyansh2005p@gmail.com>
2025-02-27 21:33:10 +0000

    Initial commit

--- server/server.js
+++ server/server.js
@@ -30,6 +30,18 @@ app.get("*", (req, res) => {
 });
 
 // API Routes
+const path = require('path');  // Make sure to import path if you haven't
+
+// Serve static files from the React app in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  // Catch-all handler for React Router (for SPAs)
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 // Get all teams data
 app.get('/alldata', async (req, res) => {
